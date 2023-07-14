e374acaa1e97331c21c31ada24213d19bb61fbed
nomercy25 <tema.polivoda@gmail.com>
2023-07-14 17:05:30 +0300

    anim added

--- js/swiper.js
+++ js/swiper.js
@@ -1,48 +1,40 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
-    direction: 'horizontal',
-    speed: 400,
-    autoplay: false,
-    loop: true,
-    effect: 'slide',
-    slidesPerView: 1,
-    spaceBetween: 11,
-
-    breakpoints: {
-      900: {
-        slidesPerView: 3,
         spaceBetween: 10,
-      },
-      480: {
-        slidesPerView: 1,
-        spaceBetween: 10,
-      },
-      800: {
-        slidesPerView: 2,
-        spaceBetween: 10,
-      },
-      700: {
-        slidesPerView: 2,
-        spaceBetween: 10,
-      },
-      1200: {
-        slidesPerView: 1,
-        spaceBetween: 10,
-      },
-    },
-    /*
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-
-    */
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    
-  });
+        loop: true,
+        freeMode: false,
+        autoplay: true,
+        speed: 700,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 0,
+            },
+            // when window width is >= 480px
+            480: {
+                slidesPerView: 1,
+                spaceBetween: 0,
+            },
+              600: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+              },
+              650: {
+                slidesPerView: 3,
+                spaceBetween: 10,
+              },
+              900: {
+                slidesPerView: 4,
+                spaceBetween: 10,
+              },
+              1200: {
+                slidesPerView: 5,
+                spaceBetween: 30,
+              },
+            },
+    });
