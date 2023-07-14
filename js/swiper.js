5d1304a19fa620399b7eae8e4d27409bab724ddc
nomercy25 <tema.polivoda@gmail.com>
2023-07-14 13:12:51 +0300

    swiper edit

--- js/swiper.js
+++ js/swiper.js
@@ -2,10 +2,34 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     speed: 400,
-    autoplay: true,
+    autoplay: false,
     loop: true,
-    spaceBetween: 30,
     effect: 'slide',
+    slidesPerView: 1,
+    spaceBetween: 11,
+
+    breakpoints: {
+      900: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      800: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      700: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1200: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+    },
     /*
     // If we need pagination
     pagination: {
